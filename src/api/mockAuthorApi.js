fe1f6a5af1ab3b9bf6ae8747afaa18bbff2e7c41
kagenyi2@gmail.com <kagenyi2@gmail.com>
2018-01-17 11:07:37 +0300

    Setting up npm scripts

--- src/api/mockAuthorApi.js
+++ src/api/mockAuthorApi.js
@@ -1,5 +1,5 @@
+/* eslint-disable no-console */
 import delay from './delay';
-
 // This file mocks a web API by working with the hard-coded data below.
 // It uses setTimeout to simulate the delay of an AJAX call.
 // All calls return promises.
